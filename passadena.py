55f836d41b63eaee8e6a4eb7a24579d8c74347f8
Paweł Betnerowicz <43805630+betnero@users.noreply.github.com>
2021-09-07 10:53:15 +0200

    Initial commit
    The code will generate a password of any length chosen by the user. 
    In case the user tries to generate a password with less than 3 characters the script will stop informing the user about the minimum characters required. 
    The script will ask after each round whether the user wants to generate  another password. 
    Using the python modue 'secrets' is a more secure solution than using the python 'random' module.

---
+++ passadena.py
@@ -0,0 +1,53 @@
+#!/bin/env python3
+
+import secrets #Python 'secrets' module is more secure and fit for use with any security related application than the python'random' module
+import string
+
+#DISCLAIMER
+print()
+print("-" * 75)
+print("DISCLAIMER: ")
+print("A password to be relatively considered secure should be at least 12 to 15 characters long. It should include special characters, upper- and lowercase letters and digits.")
+print("-" * 75)
+print("ATTENTION!: ")
+print("This password generator is not and should not be considered fully secure. It is to be treated with caution and as an assistive measure only.")
+print("The user takes full responsibility of any direct or indirect damage inflicted by proper or inproper use of the software or its parts.")
+print("-" * 75)
+print()
+
+# Looped to enable the user to run the script again or quit.
+while True:
+    
+    # User input placed in a var
+    usr = input("How long do you want the password to be? (MINIMUM 3) ")
+    # Turn user input into an integer
+    usr = int(usr)
+    print()
+    if usr < 3:
+    	print("Try more than 2 characters...")
+    	break
+
+    # Definition of scope of numbers to be used for password generation
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+
+    # Looped to generate a password with min. requirements of: at least 1 uppercase, 1 lowercasr and 1 digit
+    while True:
+
+        # Join all parts of the variable "alphabet" and itterate the number of times indicated by the user input
+        password = "".join(secrets.choice(alphabet) for i in range(usr))
+
+        # Check if the password meets minimum requirements
+        if (any(i.islower() for i in password)
+                and any(i.isupper() for i in password)
+                and sum(i.isdigit() for i in password) >= 1):
+            break
+
+    # Print the result
+    print(password)
+    print()
+
+    # Ask the user if another password will be generated or quit
+    go_again = input("Want to generate another password?... (y/n)") 
+    if go_again != "y":
+        if go_again != "Y":
+            break
